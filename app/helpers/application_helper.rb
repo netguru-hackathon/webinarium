9476b1d509d03f462b37b8ecf767effdba9f4a0d
MSzweda <malgorzata.szweda@netguru.pl>
2014-10-18 17:40:59 +0200

    add base layout elements

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+
+  def flash_msg_class(name)
+    css_classes_for_flash.fetch(name.to_sym, 'info')
+  end
+
+  private
+
+  def css_classes_for_flash
+    {
+      notice: 'success',
+      error: 'danger',
+      alert: 'warning',
+      info: 'info'
+    }
+  end
+
 end
