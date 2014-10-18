48a13a1e43bad915e59769897d0289fbeffebdcb
MSzweda <malgorzata.szweda@netguru.pl>
2014-10-18 18:33:38 +0200

    display options

--- app/models/webinar.rb
+++ app/models/webinar.rb
@@ -7,6 +7,7 @@ class Webinar
   field :youtube_url, type: String
   field :blogpost_url, type: String
   field :doc_url, type: String
+  field :code_url, type: String
   field :language, type: String
   field :translation_available, type: Mongoid::Boolean
 
