3c0a8a9da0ff843bcf1a92900e08d6f6e16012af
MSzweda <malgorzata.szweda@netguru.pl>
2014-10-19 16:35:16 +0200

    add authentication helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,31 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :current_user, :authenticated?, :sign_in, :sign_out
+
+  before_filter :authentication_check
+
+  expose(:current_user) { get_current_user }
+
+  private
+
+  def get_current_user
+    User.find(session[:user_id]) if session[:user_id].present?
+  end
+
+  def authenticated?
+    current_user.present?
+  end
+
+  def sign_in(user)
+    session[:user_id] = user.id
+  end
+
+  def sign_out
+    session[:user_id] = nil
+  end
+
+  def authentication_check
+    redirect_to auth_path unless authenticated?
+  end
 end
