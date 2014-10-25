21ecc57bdd9a3c487e382db7ae04d65840f2e6ae
MSzweda <malgorzata.szweda@netguru.pl>
2014-10-25 11:32:52 +0200

    add missing description to factory

--- spec/factories/webinar.rb
+++ spec/factories/webinar.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :webinar do
     title 'some title'
+    description 'some description'
     youtube_url 'https://www.youtube.com/watch?v=dummyid'
     language 'PL'
   end
