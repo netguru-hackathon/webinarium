5b0a0282962a752f6f3c628d5bf542d0c2197c6a
MSzweda <malgorzata.szweda@netguru.pl>
2014-10-25 11:55:15 +0200

    add some respond specs for webinar

--- spec/models/webinar_spec.rb
+++ spec/models/webinar_spec.rb
@@ -2,5 +2,14 @@ require 'rails_helper'
 
 describe Webinar do
 
+  it { should respond_to(:id) }
+  it { should respond_to(:title) }
+  it { should respond_to(:description) }
+  it { should respond_to(:language) }
+  it { should respond_to(:translation_available) }
+  it { should respond_to(:youtube_url) }
+  it { should respond_to(:blogpost_url) }
+  it { should respond_to(:doc_url) }
+  it { should respond_to(:code_url) }
 
 end
