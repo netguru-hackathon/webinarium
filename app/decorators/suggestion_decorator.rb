ca2d68ffa1231685fb0adef0d8d7b8d091899c6d
MSzweda <malgorzata.szweda@netguru.pl>
2014-10-25 16:19:08 +0200

    add suggestion decorator

---
+++ app/decorators/suggestion_decorator.rb
@@ -0,0 +1,22 @@
+class SuggestionDecorator < Draper::Decorator
+  include Draper::LazyHelpers
+
+  delegate :description, :id
+
+  def vote_count
+    object.suggestion_votes.count
+  end
+
+  def voted?(user)
+    object.suggestion_votes.pluck(:user_id).include? user.id
+  end
+
+  def upvote_link(user)
+    unless voted?(user)
+      link_to I18n.t('suggestions.suggestion.vote'), suggestion_suggestion_votes_path(suggestion_id: object.id),
+        method: :post, class: 'btn btn-info'
+    end
+  end
+
+
+end
