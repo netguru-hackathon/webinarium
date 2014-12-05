a1c547bc4840f05b890edd8b855ff2cb3d0a05c9
sufleR <szymon.fracczak@netguru.co>
2014-12-05 16:40:47 +0100

    markdown helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,17 @@
 module ApplicationHelper
 
+  def markdown(text)
+    options = { hard_wrap: true,
+                filter_html: true,
+                autolink: true,
+                no_intraemphasis: true,
+                fenced_code: true,
+                gh_blockcode: true
+    }
+    mdwn = ::Redcarpet::Markdown.new(Redcarpet::Render::HTML, options)
+    mdwn.render(text).html_safe
+  end
+
   def flash_msg_class(name)
     css_classes_for_flash.fetch(name.to_sym, 'info')
   end
