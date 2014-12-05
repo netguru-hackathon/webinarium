81d23653ddfee3e2a0a4c63d9c777bbe9456c4f7
MSzweda <malgorzata.szweda@netguru.pl>
2014-12-05 17:28:15 +0100

    add service for checking upcoming webinars

---
+++ app/services/schedule_checker.rb
@@ -0,0 +1,15 @@
+module Services
+  class ScheduleChecker
+
+    def self.send_notifs_for_near_webinars
+      near_webinars =  Webinar.where(planned_date: Time.now...30.minutes.from_now)
+      return unless near_webinars.any?
+      near_webinars.each do |webinar|
+        how_long_from_now = (webinar.planned_date - Time.now) / 60
+        msg = "Webinar '#{webinar.title}' will go on air in #{how_long_from_now} minutes"
+        Services::SlackNotifier.new(msg).call
+      end
+    end
+
+  end
+end
